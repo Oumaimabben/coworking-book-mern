e67af864fcc5d3b68c27156f71df61784a355e6f
Oumaimabben <obenothmen865@gmail.com>
2024-04-13 03:11:34 +0200

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
 
 export default function App() {
-  return (
-    <div>
-      APP
-    </div>
-  )
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path='/' element={< Home/>}/>
+    <Route path='/login' element={< Login/>}/>
+    <Route path='/register' element={< Register/>}/>
+  </Routes>
+  </BrowserRouter>
 }
